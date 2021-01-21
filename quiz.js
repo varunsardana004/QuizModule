fb64fb92eaca164b603bbb09777fb110e1200968
Varun Sardana <54537819+varunsardana004@users.noreply.github.com>
2021-01-21 23:59:19 +0530

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,83 @@
+
+var i=1;
+var j=0;    
+let questions = [
+    {
+        question: "A complete graph can have?",
+        choiceA : "no edge",
+        choiceB : "2 spanning trees",
+        choiceC : "disconnected verticese",
+        choiceD : "multiple loops",
+        correct : "B"
+    },
+    {
+        question : "Which type of JavaScript language is ___",
+        choiceA : "Object-Oriented",
+        choiceB : "Object-Based",
+        choiceC : "Assembly-language",
+        choiceD : "High-level",
+        correct : "B"
+    },{
+        question : "Which one of the following also known as Conditional Expression:",
+        choiceA : "Alternative to if-else",
+        choiceB : "Switch statement",
+        choiceC : "If-then-else statement",
+        choiceD : "immediate if",
+        correct : "D"
+    },{
+        question : "In JavaScript, what is a block of statement?",
+        choiceA : "Conditional block",
+        choiceB : "combines a number of statements into single compound statement",
+        choiceC : "both conditional block and a single statement",
+        choiceD : "block that contains a single statement",
+        correct : "B"
+    },
+    {
+        question : "When interpreter encounters an empty statements, what it will do:",
+        choiceA : "Shows a warning",
+        choiceB : "Prompts to complete the statement",
+        choiceC : "Throws an error",
+        choiceD : "Ignores the statements",
+        correct : "D"
+    },
+    {
+        question : "The 'function' and ' var' are known as:",
+        choiceA : "Keywords",
+        choiceB : "Data types",
+        choiceC : "Declaration statements",
+        choiceD : "Prototypes",
+        correct : "C"
+    },
+    {
+        question : "Which of the following variables takes precedence over the others if the names are         the same?",
+        choiceA : "Global variable",
+        choiceB : "The local element",
+        choiceC : "The two of the above",
+        choiceD : "None of the above",
+        correct : "B"
+    },
+    {
+        question : "Which one of the following is the correct way for calling the JavaScript code?",
+        choiceA : "Preprocessor",
+        choiceB : "Triggering Event",
+        choiceC : "RMI",
+        choiceD : "Function/Method",
+        correct : "D"
+    },
+    {
+        question : "Which of the following type of a variable is volatile?",
+        choiceA : "Mutable variable",
+        choiceB : "Dynamic variable",
+        choiceC : "Volatile variable",
+        choiceD : "Immutable variable",
+        correct : "C"
+    },
+    {
+        question : "When there is an indefinite or an infinite value during an arithmetic computation          in a program, then JavaScript prints______.",
+        choiceA : "Prints an exception error",
+        choiceB : "Prints an overflow error",
+        choiceC : "Displays 'Infinity' ",
+        choiceD : "Prints the value as such",
+        correct : "C"
+    }
+];
